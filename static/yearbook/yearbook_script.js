1bfb2e312d6dd9378b10cef131f0f3c5dd0db129
disconnect101 <adityagupta4114@gmail.com>
2020-06-19 00:22:19 +0530

    image view

--- static/yearbook/yearbook_script.js
+++ static/yearbook/yearbook_script.js
@@ -1,7 +1,6 @@
 arrlist = [];
 year = null;
-counter = 0;
-fulllength = 0;
+
 
 $(document).ready(function () {
     arrlist = $('.regno-list-item');
@@ -20,12 +19,7 @@ $(document).ready(function () {
 
 });
 
-$(window).scroll(function () {
-    if(($(window).scrollTop() + $(window).height() == $(document).height()) && $('#imagelist').css('display')=='block'){
-        for(var i=1 ; i<=6 && counter<fulllength ; i++, counter++)
-            $('#imagelist').append('<div style="padding: 1em;margin: 1em auto; font-size: 15px; border: 5px solid lightgrey; height: 270px" class="col-lg-2 col-md-3 col-sm-4 col-xs-6 text-center custom-font-2"><img  style="width: 100%;max-height: 180px ; margin-bottom: 0.5em;" src="' + arrlist[counter].childNodes[1].innerText + '"><b>' + arrlist[counter].childNodes[2].innerText + '</b><br>' + arrlist[counter].childNodes[0].innerText + '</div>');
-    }
-});
+
 
 
 
