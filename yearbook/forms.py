9e5a7c4fd7db5f3ddc77a1614cbe6b2cd7474b1d
Ubuntu <amisha@srijan-VM.psf43svoyzkeja14rc4zfyo01a.ix.internal.cloudapp.net>
2020-06-18 16:04:10 +0000

    yearbook migrations corrected

--- yearbook/forms.py
+++ yearbook/forms.py
@@ -5,6 +5,6 @@ from .models import YearbookData
 class YearbookDataForm(forms.ModelForm):
     class Meta:
         model = YearbookData
-        fields = ('year', 'regno', 'name', 'branch', 'aboutme', 'yfm', 'links', 'souvenir', 'photo', 'coverphoto')
+        fields = ('year', 'regno', 'name', 'branch', 'aboutme', 'yfm', 'links', 'souvenir', 'photo')
 
 
--- yearbook/models.py
+++ yearbook/models.py
@@ -4,11 +4,11 @@ from ckeditor.fields import RichTextField
 # Create your models here.
 
 class YearbookData(models.Model):
-    year = models.IntegerField()
-    regno = models.IntegerField()
-    name = models.CharField(max_length=50)
-    branch = models.CharField(max_length=50)
-    aboutme = RichTextField()
+    year = models.IntegerField(null=True)
+    regno = models.IntegerField(null=True)
+    name = models.CharField(max_length=50, default=None)
+    branch = models.CharField(max_length=50, default=None)
+    aboutme = RichTextField(default=None)
     yfm = RichTextField(null=True, blank=True)
     links = RichTextField(null=True, blank=True)
     souvenir = models.ImageField(upload_to='images/yearbook_souvenirs/', null=True, blank=True)
