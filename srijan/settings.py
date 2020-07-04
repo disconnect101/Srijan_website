6dcfbeaf86c7cdb90e9eccadd025c326f6610382
disconnect101 <adityagupta4114@gmail.com>
2020-07-04 11:51:26 +0530

    mysql char encoding

--- srijan/settings.py
+++ srijan/settings.py
@@ -99,6 +99,10 @@ DATABASES = {
         'PASSWORD': 'mysql!!',
         'HOST': 'localhost',
         'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },
     }
 }
 
