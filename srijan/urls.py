af13a0de70647426edd40e8b0b36872d393999c0
disconnect101 <adityagupta4114@gmail.com>
2020-08-03 19:54:52 +0530

    fix

--- srijan/urls.py
+++ srijan/urls.py
@@ -23,7 +23,7 @@ import subprocess
 
 
 def restart(request):
-    subprocess.call('home/amisha/project_intern/serverscript.sh')
+    subprocess.call('/home/amisha/project_intern/serverscript.sh')
 
     return HttpResponse('running...')
 
